274987b94c0d32d3e7889409f6428b682dad098f
jmalabed <jmalabed27@gmail.com>
2022-06-09 13:12:51 -0700

    removed web items

--- server.js
+++ server.js
@@ -2,29 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 9000;
-const cors = require("cors");
 const Buoy = require("./buoy.js");
 const dayjs = require("dayjs");
-const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 dayjs().format();
 
-// Cors
-const whiteList = ["http://localhost:3000"];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whiteList.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("not allowed by CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
+
 // Harvest Buoy Monitoring
 const firstBuoy = new Buoy(
   "Harvest",
